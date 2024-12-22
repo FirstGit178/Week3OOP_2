e3bf723d3ddaca34a7d18c65498da7127d719876
Fergal McNamara <g00180082@atu.ie>
2024-12-22 20:21:17 +0000

    Initial Calc commit **add two numbers**

---
+++ src/main/java/ie/atu/week3/Calc.java
@@ -0,0 +1,43 @@
+package ie.atu.week3;
+
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+
+        // Prompt the user for a number
+        // Prompt the user for a second number
+        // Display the total to the screen
+        Scanner scan1 = new Scanner(System.in);
+        // Please enter first number
+        System.out.println("Please enter a number: ");
+        double firstNum = scan1.nextDouble();
+        System.out.println("You entered " + firstNum);
+
+        // Please enter second number
+        System.out.println("Please enter second number: ");
+        double secondNum = scan1.nextDouble();
+        System.out.println("You entered " + secondNum);
+
+        //Please select an operation (add, subtract, multiply, divide )
+        System.out.print("Choose an operation \n(add, subtract, multiply, divide ): ");
+        String operation = scan1.next();
+
+        //Perform the operation
+        boolean validOperation = true;
+        double result = switch (operation){
+
+            // addition
+            case "add" -> {
+                System.out.println("The result is " + (firstNum + secondNum));
+                yield firstNum + secondNum;
+            }
+            // default if anything except valid option is selected //yield in default case
+            default -> {
+                System.out.println("Invalid operation.");
+                yield 0;
+            }
+
+        };
+    }
+}
